a6a413e8717f60e694bf511a03b0350663d243f8
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-03-03 18:27:31 +0000

    Loaded store1 from database and assigned it to instance variable

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -14,6 +14,8 @@ puts "Store1: ", @store1.name
 @store1 = Store.update(1, name: 'Vancouver')
 puts "New Store1: ", @store1.name
 
+
+
 # Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
 # Load the second store from the database and assign it to @store2.
 # Update the first store (@store1) instance in the database. (Change its name or something.)
