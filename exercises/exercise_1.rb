92b927d4310b76010c13c6d0610312e89e0494cf
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-03-03 18:13:36 +0000

    using active record to create class method multiple time to create 3 stores in the database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -10,6 +10,8 @@ store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true
 
 puts "Store.count: ", Store.count
 
+
+
 # Burnaby (annual_revenue of 300000, carries men's and women's apparel)
 # Richmond (annual_revenue of 1260000 carries women's apparel only)
 # Gastown (annual_revenue of 190000 carries men's apparel only)
